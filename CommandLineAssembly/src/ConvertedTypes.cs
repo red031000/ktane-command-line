6e762d1f15c5aacaad182f597bafca6ea5025021
BakersDozenBagels <bakersdozenbagelsbergman@gmail.com>
2021-05-22 00:33:05 -0400

    Made the solve commands use the TP autosolver (if available)

--- CommandLineAssembly/src/ConvertedTypes.cs
+++ CommandLineAssembly/src/ConvertedTypes.cs
@@ -3,28 +3,18 @@ using System.Reflection;
 
 public static class CommonReflectedTypeInfo
 {
-	static CommonReflectedTypeInfo()
-	{
-		HandlePassMethod = typeof(BombComponent).GetMethod("HandlePass", BindingFlags.NonPublic | BindingFlags.Instance);
-		GameRecordCurrentStrikeIndexField = typeof(GameRecord).GetField("currentStrikeIndex", BindingFlags.NonPublic | BindingFlags.Instance);
-		UpdateTimerDisplayMethod = typeof(TimerComponent).GetMethod("UpdateDisplay", BindingFlags.NonPublic | BindingFlags.Instance);
-	}
+    private const BindingFlags Flags = BindingFlags.NonPublic | BindingFlags.Instance;
 
-	public static MethodInfo HandlePassMethod
-	{
-		get;
-		private set;
-	}
+    static CommonReflectedTypeInfo()
+    {
+        HandlePassMethod = typeof(BombComponent).GetMethod("HandlePass", Flags);
+        GameRecordCurrentStrikeIndexField = typeof(GameRecord).GetField("currentStrikeIndex", Flags);
+        UpdateTimerDisplayMethod = typeof(TimerComponent).GetMethod("UpdateDisplay", Flags);
+    }
 
-	public static MethodInfo UpdateTimerDisplayMethod
-	{
-		get;
-		private set;
-	}
+    public static MethodInfo HandlePassMethod { get; private set; }
 
-	public static FieldInfo GameRecordCurrentStrikeIndexField
-	{
-		get;
-		private set;
-	}
+    public static MethodInfo UpdateTimerDisplayMethod { get; private set; }
+
+    public static FieldInfo GameRecordCurrentStrikeIndexField { get; private set; }
 }
